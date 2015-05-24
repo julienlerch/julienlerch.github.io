f6e45f5f9d1c955366e0adbdb87bcd8c53093a3d
julienlerch <julienlerchperso-github@yahoo.fr>
2015-05-24 12:11:52 +0200

    uodate GA tracker

--- cv/cv.js
+++ cv/cv.js
@@ -4,7 +4,7 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-6113935-2', 'free.fr');
+ga('create', 'UA-63345015-1', 'free.fr');
 ga('send', 'pageview');
 
 (function() {
